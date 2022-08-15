29d3b35e292b58056fea7709208751d8f35d6562
joyMohanta <joyroy1062@gmail.com>
2022-08-16 01:24:03 +0600

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Navbar = () => {
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        width: "12ch",
+        "&:focus": {
+          width: "20ch",
+        },
+      },
+    },
+  }));
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "block", sm: "block" } }}
+            >
+              PixaBay Image Finder
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/SearchPhoto/SearchPhoto.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import axios from "axios";
+
+class SearchPhoto extends Component {
+  state = {
+    searchText: "",
+    amount: 10,
+    apiUrl: "https://pixabay.com/api",
+    apiKey: "29163420-480b4517f6957dfc570a8d2b2",
+    images: [],
+  };
+
+  onTextChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      axios
+        .get(
+          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+        )
+        .then((res) => this.setState({ images: res.data.hits }))
+        .catch((err) => console.log(err));
+    });
+  };
+
+  render() {
+    console.log(this.state.images);
+    return (
+      <div>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "400px" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+            name="searchText"
+            value={this.state.searchText}
+            onChange={this.onTextChange}
+          />
+        </Box>
+
+        <Box sx={{ minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-label">Show Result</InputLabel>
+          <Select
+            name="amount"
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </Box>
+      </div>
+    );
+  }
+}
+export default SearchPhoto;
